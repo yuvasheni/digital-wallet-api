402cbe9b4c95d061c06bec8580a77ad07a155afa
yuvasheni <sheniyuva@gmail.com>
2025-09-22 20:21:22 +0800

    Initial Spring Boot project setup

---
+++ src/test/java/com/digitalwallet/api/DigitalWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalwallet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalWalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
